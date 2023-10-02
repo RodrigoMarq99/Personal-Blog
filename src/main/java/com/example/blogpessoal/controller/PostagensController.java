076646fefa7d44738a71d4cb23bb866efb904991
RodrigoMarq99 <rodrigo.marquesoliveira@hotmail.com>
2023-10-02 12:37:11 -0300

    Projeto de blog pessoal com Spring

--- src/main/java/com/example/blogpessoal/controller/PostagensController.java
+++ src/main/java/com/example/blogpessoal/controller/PostagensController.java
@@ -1,17 +1,24 @@
 package com.example.blogpessoal.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 import com.example.blogpessoal.model.Postagens;
 import com.example.blogpessoal.repository.PostagensRepository;
+import jakarta.validation.Valid;
 
 @RestController									   // Informar ao Spring que essa classe será um controlador da nossa API Rest
 @RequestMapping("/postagens")					   // HTTP
@@ -34,5 +41,36 @@ public class PostagensController {
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
 	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagens>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagensRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagens> post(@Valid @RequestBody Postagens postagem){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagensRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagens> put(@Valid @RequestBody Postagens postagem){
+		return postagensRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+					.body(postagensRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagens> postagem = postagensRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagensRepository.deleteById(id);
+	}
+	
 }
 	
--- src/main/java/com/example/blogpessoal/repository/PostagensRepository.java
+++ src/main/java/com/example/blogpessoal/repository/PostagensRepository.java
@@ -1,11 +1,14 @@
 package com.example.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.blogpessoal.model.Postagens;
 
 public interface PostagensRepository extends JpaRepository<Postagens,Long>{
-
-	
+	// Para retornar todos os itens títulos que eu tenho. Ele fala o que tenho dentro de título
+	public List<Postagens> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	
 }
